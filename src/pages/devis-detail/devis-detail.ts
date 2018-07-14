4999e6c0bec98bae4742e77fc645a17b0ee599dc
angemarie <ange-m.tellier@laposte.net>
2018-07-14 15:07:32 +0200

    Add get devis composants on edit form
    Ajout d'une liste de composants au devis 1
    Réccuperation de la liste des composant existant
    Réccupération des composants du devis et affichage dans le formulaire d'édition

--- src/pages/devis-detail/devis-detail.ts
+++ src/pages/devis-detail/devis-detail.ts
@@ -3,6 +3,7 @@ import { NavController, NavParams, Events } from 'ionic-angular';
 
 import moment from 'moment';
 import 'moment/locale/fr';
+import _ from 'underscore';
 
 import {DevisProvider} from "../../providers/devis/devis";
 import {ComposantProvider} from "../../providers/composant/composant"
@@ -25,21 +26,45 @@ export class DevisDetailPage {
     etatDevis: 'En attente',
     prixDevis: 0
   };
+  composantsDevis: any = [];
+  composantList: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public events: Events, public devisService: DevisProvider, public composantService: ComposantProvider) {
+  constructor(
+    public navCtrl: NavController, public navParams: NavParams, public events: Events, 
+    public devisService: DevisProvider, public composantService: ComposantProvider
+  ) {
     this.devis.idDevis = navParams.get('idDevis');
+    this.getAllComposant();
   }
 
   ionViewDidLoad() {
     this.getDevis(this.devis.idDevis);
   }
 
-  getDevis(idDevis) {
-    this.devisService.getOne(idDevis).then((res) => {
-      this.devis = res;
+  getAllComposant() {
+    this.composantService.getAll().then((res) => {
+      this.composantList = res;
     }, (err) => {
       console.log(err);
     });
+  }
+
+  getDevis(idDevis) {
+    this.devisService.getOne(idDevis).then((res) => {
+      this.devis = res;
+      this.getComposantDevis();
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  getComposantDevis() {
+    _.each(this.devis.composants, function(v,k){
+      this.composantsDevis.push({
+        nb: k+1,
+        composant: v
+      });
+    }, this);
   }
 
   updateDevis() {
